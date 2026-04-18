186a37ba5dde332857aa00d41e1090afd099e985
rmdkarhdwn <dkgkrlrnlvksgsk@gmail.com>
2026-04-18 19:00:38 +0900

    보이는걸 app.tsx에서 MainScreen쪽으로 보이게 바꾸고 콘솔도 정상적으로 뜨게 함

--- src/hooks/useGetId.ts
+++ src/hooks/useGetId.ts
@@ -2,13 +2,12 @@ import { useEffect } from "react";
 import { getPosts } from "../api/posts";
 
 export default function useGetId() {
+    console.log("훅 실행됨");
     useEffect(() => {
         async function a() {
-            try {
-                console.log(await getPosts());
-            } catch (error) {
-                console.error(error)
-            }
+            const posts = await getPosts();
+            const postId = posts.map((post) => post.id);
+            console.log(postId);
         }
         a();
     },[])
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -2,8 +2,9 @@ import useGetId from "../hooks/useGetId"
 
 
 export default function Main() {
+    console.log("Main 렌더링됨");
     useGetId();
     return (
-        <div></div>
+        <div>main</div>
     )
 }
