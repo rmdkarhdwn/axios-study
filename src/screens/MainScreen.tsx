84a48450ff1d4b507225c97be5728dbbff72a3e8
rmdkarhdwn <dkgkrlrnlvksgsk@gmail.com>
2026-04-20 11:36:39 +0900

    MainScreen 코드 선택 기능

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,8 +1,10 @@
+import {useState} from "react";
 import useGetId from "../hooks/useGetId"
 import { MainContainer,Button } from "../styles/MainScreen.style";
 
 
 export default function Main() {
+    const [selectedName,setSelectedName] = useState<string | null>(null);
     const { names, isLoading, error } = useGetId();
     if (isLoading) {
         return <div>로딩중...</div>
@@ -12,7 +14,12 @@ export default function Main() {
     }
     return (
         <MainContainer>
-            {names &&names.map((n)=> <Button selected={true} key={n}>
+            {names &&names.map((n)=>
+                <Button
+                    selected={selectedName === n}
+                    key={n}
+                    onClick={()=>setSelectedName(n)}
+                >
                 {n}
             </Button>)}
         </MainContainer>
